d1bb80a335da97de847a72628af6d8a52bcc9518
hafsaqamar87 <60553705+hafsaqamar87@users.noreply.github.com>
2020-07-19 18:55:30 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Cards, Chart, CountryPicker} from './components';
+import { fetchData } from './api/index';
+import styles from './App.module.css'; 
+import image from './images/COVID-19.jpg';
+
+class App extends React.Component{
+  state ={
+    data:{}
+  }
+async componentDidMount(){
+    const data =await fetchData();
+    this.setState({data});
+  }
+   handleCountryChange = async (country) =>{
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+   }
+  render(){
+    const {data ,country}=this.state;
+    return(
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
+
